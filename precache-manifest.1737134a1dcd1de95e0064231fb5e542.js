c7c65283ab1df17e7a3b16ae898760e8bc171927
Matias Rivas <matiutu98@gmail.com>
2020-10-04 22:05:44 -0300

    Updates

---
+++ precache-manifest.1737134a1dcd1de95e0064231fb5e542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2ac0ec5546e5fa882b644b4741ae1b",
+    "url": "/proyecto-modulo3_frontend-deploy/index.html"
+  },
+  {
+    "revision": "d0cd619571b65711c4a5",
+    "url": "/proyecto-modulo3_frontend-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e57ca5969dcc46d7497",
+    "url": "/proyecto-modulo3_frontend-deploy/static/js/2.cb927320.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/proyecto-modulo3_frontend-deploy/static/js/2.cb927320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0cd619571b65711c4a5",
+    "url": "/proyecto-modulo3_frontend-deploy/static/js/main.87771c63.chunk.js"
+  },
+  {
+    "revision": "7fbc8c2c85695a71158a",
+    "url": "/proyecto-modulo3_frontend-deploy/static/js/runtime-main.b7313169.js"
+  }
+]);
